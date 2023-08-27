fd0a19512074f8a5872cbfbbb5ae5123061d75bb
ShiroksX <60487133+ShiroksX@users.noreply.github.com>
2023-08-27 12:55:38 +0200

    a

--- src/main/kotlin/name/modid/ExampleMod.kt
+++ src/main/kotlin/name/modid/ExampleMod.kt
@@ -11,6 +11,5 @@ object ExampleMod : ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 		logger.info("Hello Fabric world!")
-		logger.info("ModLoaded.")
 	}
 }
