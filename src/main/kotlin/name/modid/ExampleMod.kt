ff2761542dcc8538e803bb1e6203645ebada7ae7
ShiroksX <60487133+ShiroksX@users.noreply.github.com>
2023-08-27 12:54:38 +0200

    a

---
+++ src/main/kotlin/name/modid/ExampleMod.kt
@@ -0,0 +1,16 @@
+package name.modid
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
+
+object ExampleMod : ModInitializer {
+    private val logger = LoggerFactory.getLogger("chibs-tech")
+
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("Hello Fabric world!")
+		logger.info("ModLoaded.")
+	}
+}
